7559d2c4d0478cfb384802c937e41d82523497b0
schazbot <charlotte.pearce1984@gmail.com>
2019-09-16 14:35:02 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(arr, str) {
+    return arr.filter(driver => driver.toLowerCase() == str.toLowerCase())
+
+}
+
+function fuzzyMatch(arr, str) { 
+    return arr.filter(name => !name.search(str))
+}
+
+function matchName(arr, str) { 
+    return arr.filter(person => person.name == str)
+}
